45c9e735c474fb41fc5bf78ab3ba2985d963a38a
AmaRocket <alexeverdie999@gmail.com>
2022-03-30 14:04:56 +0300

    2

--- django_rest_api/django_rest_api/settings.py
+++ django_rest_api/django_rest_api/settings.py
@@ -149,7 +149,7 @@ CELERY_BROKER_URL = "redis://redis:6379"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Heroku: Update database configuration from $DATABASE_URL.
-# import dj_database_url
-#
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
